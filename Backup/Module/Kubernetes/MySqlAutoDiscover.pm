a8c66a952dfdad0ffc5c4ac670debb8970216a58
ralph <github@ralph-schuster.eu>
2022-09-18 07:01:18 +0000

    Fix export / avoid using a log

--- Backup/Module/Kubernetes/MySqlAutoDiscover.pm
+++ Backup/Module/Kubernetes/MySqlAutoDiscover.pm
@@ -80,14 +80,20 @@ sub getSchemas {
 
 	# otherwise return all instances defined
 	my $podName  = $self->getPodName($svc->{metadata}->{name}.'-backup');
-	my $cmd = "run $podName -n ".$svc->{metadata}->{namespace}." -ti --image=mariadb --restart=Never --rm -- ".
-		"mysql \"--user=".$self->{config}->{username}."\"".
-		" \"--password=".$self->{config}->{password}."\"".
+	my $cmd = "run $podName -n ".$svc->{metadata}->{namespace}." -ti --image=mariadb --restart=Never --rm -- bash -c \"sleep 3 \&\& ".
+		"mysql ".
+		" --user=".$self->{config}->{username}.
+		" --password=".$self->{config}->{password}.
 		" --host=".$svc->{metadata}->{name}.'.'.$svc->{metadata}->{namespace}.'.svc.cluster.local '.
 		" --port=".@{$svc->{spec}->{ports}}[0]->{port}.
 		" --batch".
-		" --skip-column-names -e \"show databases\"";
+		" --skip-column-names -e \\\"show databases\\\"".
+		"\"";
 	my @LINES = $self->{main}->invokeKubectl($cmd, 'lines');
+	if (!defined(scalar(@LINES))) {
+#		$self->{log}->error("Cannot retrieve database list");
+#		return ();
+	}
 
 	my $line;
 	foreach $line (@LINES) {
@@ -119,63 +125,39 @@ sub exportDatabase {
 	my $svc    = shift;
 	my $schema = shift;
 
-	# We need to create a job
-	my $jobName  = $self->getPodName($svc->{metadata}->{name}.'-backup');
-	# $self->{log}->info($jobName);
-	my $yamlfile      = "/tmp/$jobName.yaml";
-	my $mysqldumpopts = defined($self->{config}->{mysqldumpopts}) ? '        - '.$self->{config}->{mysqldumpopts}."\n" : '';
-	if (open(FOUT, ">$yamlfile")) {
-		print FOUT "apiVersion: batch/v1\n".
-			"kind: Job\n".
-			"metadata:\n".
-			"  name: $jobName\n".
-			"  namespace: ".$svc->{metadata}->{namespace}."\n".
-			"spec:\n".
-			"  template:\n".
-			"    spec:\n".
-			"      containers:\n".
-			"      - name: mysqldump\n".
-			"        image: mariadb\n".
-			"        command:\n".
-			"        - mysqldump \n".
-			"        - --user=".$self->{config}->{username}."\n".
-			"        - --password=".$self->{config}->{password}."\n".
-			"        - --host=".$svc->{metadata}->{name}.'.'.$svc->{metadata}->{namespace}.".svc.cluster.local\n".
-			"        - --port=".@{$svc->{spec}->{ports}}[0]->{port}."\n".
-			"        - --quote-names\n".
-			$mysqldumpopts.
-			"        - --skip-lock-tables\n".
-			"        - --opt\n".
-			"        - --databases\n".
-			"        - $schema\n".
-			"      restartPolicy: Never\n";
-		close(FOUT);
-	}
-
+	my $dumpfile      = tmpnam().'.sql';
+	my $podName       = $self->getPodName($svc->{metadata}->{name}.'-backup');
+	my $mysqldumpopts = defined($self->{config}->{mysqldumpopts}) ? $self->{config}->{mysqldumpopts} : '';
+	my $cmd           = "run $podName -n ".$svc->{metadata}->{namespace}." -ti --image=mariadb --restart=Never --rm -- bash -c \"sleep 3 \&\& ".
+		"mysqldump".
+		" --user=".$self->{config}->{username}.
+		" --password=".$self->{config}->{password}.
+		" --host=".$svc->{metadata}->{name}.'.'.$svc->{metadata}->{namespace}.".svc.cluster.local".
+		" --port=".@{$svc->{spec}->{ports}}[0]->{port}.
+		" --quote-names".
+		" $mysqldumpopts".
+		" --skip-lock-tables".
+		" --opt".
+		" --databases".
+		" $schema".
+		"\"".
+		" >$dumpfile";
 	# $self->{log}->info($yamlfile);
 
-	# Create the job
 	if (!$self->{config}->{dryRun}) {
-		$self->{main}->invokeKubectl("create -f $yamlfile", 'lines');
-		#unlink($yamlfile);
-
-		# Wait for completion
-		my $condition = $self->{main}->invokeKubectl("wait --for=condition=complete --timeout=30s job/$jobName -n ".$svc->{metadata}->{namespace});
-		if ($condition->{status}->{succeeded}) {
-			my $dumpfile = tmpnam().'.sql';
-			$self->{main}->invokeKubectl("logs job/$jobName -n ".$svc->{metadata}->{namespace}." >$dumpfile", 'lines');
+		my $rc = $self->{main}->invokeKubectl($cmd, 'lines');
+		if (defined($rc)) {
 			# Return the dumpfile
 			return $dumpfile;
 		} else {
-			# Job failed
+			# Export failed
+			unlink($dumpfile);
 			return 0;
 		}
 	}
 	
-	#unlink($yamlfile);
-	# Return smth when in dry mode
 	if ($self->{config}->{dryRun}) {
-		return tmpnam().'.sql';
+		return $dumpfile;
 	}
 	return 0;
 }
